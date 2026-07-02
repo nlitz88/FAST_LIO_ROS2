38eb4252e9c4c84247cd289732d5ff6e3043df3e
Nathan Litzinger <nlitz88@gmail.com>
2026-07-02 00:54:58 -0400

    Exposed velocity

--- src/IMU_Processing.hpp
+++ src/IMU_Processing.hpp
@@ -57,6 +57,7 @@ class ImuProcess
   V3D cov_bias_gyr;
   V3D cov_bias_acc;
   double first_lidar_time;
+  V3D get_angvel_last() const { return angvel_last; }
 
  private:
   void IMU_init(const MeasureGroup &meas, esekfom::esekf<state_ikfom, 12, input_ikfom> &kf_state, int &N);
